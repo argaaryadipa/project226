deb32ee6f5e01ada32fb30512e80adb16ad303a4
argaaryadipa <argaaryadipa10@gmail.com>
2021-04-03 03:12:58 -0700

    update file

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,53 @@
 |
 */
 
+// routing default
+
+use App\Http\Controllers\BioArgaController;
+
 Route::get('/', function () {
-    return view('upload');
+    return view('welcome');
 });
+
+Route::get('/mahasiswa', function (){
+    echo 'Mahasiswa';
+});
+
+Route::get('/ubg/ilkom/mahasiswa', function (){
+    return 'Mahasiswa Ilkom Kampus UBG';
+});
+
+
+// routing dengan parameter
+Route::get('/ubg/ilkom/mahasiswa/{semester}/{nim}', function ($semester, $nim){
+    return 'Mahasiswa Ilkom Kampus UBG Semester : ' . $semester . '-NIM : ' . $nim;
+});
+
+
+// route redirect
+Route::redirect('/login', '/masuk');
+
+
+// route group
+Route::group(['prefix' => '/admin'], function() {
+    Route::get('/mahasiswa', function(){
+        return 'Admin Mahasiswa';
+    });
+    Route::get('/dosen', function(){
+        return 'Admin Dosen';
+    });
+    Route::get('/staf', function(){
+        return 'Admin Staf';
+    });
+});
+
+
+Route::get('/', function () {
+    return view('welcome');
+})-> name('home');
+
+Route::get('/portofolio-saya', function () {
+    return view('profile');
+})-> name('porto');
+
+Route::get('/identitas-saya', 'BioArgaController@index')-> name('iden');
